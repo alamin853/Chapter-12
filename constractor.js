eb4940aa32f25c6a5114b752e7873028721a6184
alamin853 <oriam853@gmail.com>
2021-03-11 12:51:49 +0600

    Constractor

---
+++ constractor.js
@@ -0,0 +1,17 @@
+var Rectangle = function(width, height){
+    this.width = width
+    this.height = height
+
+    this.draw = function(){
+        console.log('I am a rectangle')
+        this.printProperties()
+    }
+
+    this.printProperties = function(){
+        console.log('My width is: ' + this.width)
+        console.log('My height is: ' + this.height)
+    }
+}
+
+var rect1 = new Rectangle(10, 9)
+rect1.draw()
